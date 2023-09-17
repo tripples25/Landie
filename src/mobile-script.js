1aa0e0bf82cd7b0385f5f71ab33f35d9dc988cbd
tripples25 <92451706+tripples25@users.noreply.github.com>
2023-09-17 22:02:43 +0500

    Fixed lock bug and cards overview

--- src/mobile-script.js
+++ src/mobile-script.js
@@ -1,7 +1,8 @@
 const navList= document.querySelector('.header-navigation-button')
 const navListContainer = document.querySelector('.hamburger-container');
+const main = document.querySelector('body');
 
 navList.addEventListener('click', () => {
     navListContainer.classList.toggle("active")
+    main.classList.toggle("scroll-lock")
 });
-
